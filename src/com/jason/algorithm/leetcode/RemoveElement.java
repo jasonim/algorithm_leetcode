a8c2074e7c0e5b2edd8726adc30ff645f9a23216
Jiandong Hu <hk081508@gmail.com>
2020-08-17 19:41:04 +0800

    Update RemoveElement.java

--- src/com/jason/algorithm/leetcode/RemoveElement.java
+++ src/com/jason/algorithm/leetcode/RemoveElement.java
@@ -1,6 +1,19 @@
 package com.jason.algorithm.leetcode;
 
 /**
+ * Given an array nums and a value val, remove all instances of that value in-place and return the new length.
+
+Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
+
+The order of elements can be changed. It doesn't matter what you leave beyond the new length.
+
+Example 1:
+
+Given nums = [3,2,2,3], val = 3,
+
+Your function should return length = 2, with the first two elements of nums being 2.
+
+It doesn't matter what you leave beyond the returned length.
  * Created by jsson on 20/2/23.
  */
 public class RemoveElement {
