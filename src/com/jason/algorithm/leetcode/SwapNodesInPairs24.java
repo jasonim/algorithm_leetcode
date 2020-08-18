b4f47686f533d0a6fb392c12265f330eca573158
Jiandong Hu <hk081508@gmail.com>
2020-08-18 11:44:33 +0800

    Update SwapNodesInPairs24.java

--- src/com/jason/algorithm/leetcode/SwapNodesInPairs24.java
+++ src/com/jason/algorithm/leetcode/SwapNodesInPairs24.java
@@ -3,6 +3,15 @@ package com.jason.algorithm.leetcode;
 import com.jason.algorithm.leetcode.base.ListNode;
 
 /**
+ Given a linked list, swap every two adjacent nodes and return its head.
+
+You may not modify the values in the list's nodes, only nodes itself may be changed.
+
+ 
+
+Example:
+
+Given 1->2->3->4, you should return the list as 2->1->4->3.
  * Created by jsson on 20/5/24.
  */
 public class SwapNodesInPairs24 {
