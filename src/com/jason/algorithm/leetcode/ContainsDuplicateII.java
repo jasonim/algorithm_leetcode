64b528f4a72717ffb8ed5ca7a0143b21e2d11ad3
Jiandong Hu <hk081508@gmail.com>
2020-09-23 21:44:11 +0800

    Update ContainsDuplicateII.java

--- src/com/jason/algorithm/leetcode/ContainsDuplicateII.java
+++ src/com/jason/algorithm/leetcode/ContainsDuplicateII.java
@@ -47,4 +47,20 @@ public class ContainsDuplicateII {
 
         return false;
     }
+ 
+    public boolean containsNearByDuplicate1(int[] nums, int k) {
+        Set<Integer> set = new HashSet<>();
+        for(int i = 0; i < nums.length; i++) {
+           if (set.contains(nums[i])) {
+              return true;
+           }
+         
+           set.add(nums[i]);
+           if (set.size() == k + 1)) {
+              set.remove(nums[i - k]);
+           }
+        }
+     
+        return false;
+    }
 }
