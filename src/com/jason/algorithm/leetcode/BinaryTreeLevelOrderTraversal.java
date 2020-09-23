5399b70b128f1a75665f130d797edf66414f63f9
Jiandong Hu <hk081508@gmail.com>
2020-09-23 14:08:30 +0800

    Update BinaryTreeLevelOrderTraversal.java

--- src/com/jason/algorithm/leetcode/BinaryTreeLevelOrderTraversal.java
+++ src/com/jason/algorithm/leetcode/BinaryTreeLevelOrderTraversal.java
@@ -39,4 +39,39 @@ public class BinaryTreeLevelOrderTraversal {
 
         return res;
     }
+    
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        ArrayList<List<Integer>> res = new ArrayList<>();
+        
+        if(root == null) {
+            return res;
+        }
+        
+        LinkedList<Pair<TreeNode, Integer>> queue = new LinkedList<>();
+        
+        queue.addLast(new Pair<>(root, 0));
+        
+        while(!queue.isEmpty()) {
+            Pair<TreeNode, Integer> front = queue.removeFirst();
+            TreeNode node = front.getKey();
+            int level = front.getValue();
+            
+            
+            if (level == res.size()) {
+                res.add(new ArrayList<Integer>); 
+            }
+            
+            res.get(level).add(node.val);
+            
+            if (node.left != null) {
+                queue.addList(new Pair<>(node.left, level + 1))};
+            }
+            
+            if (node.right != null) {
+                queue.addList(new Pair<>(node.right, level + 1));
+            }
+        }
+    
+        return res;
+    }
 }
